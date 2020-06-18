0deaee85657e13e6f342835b7f98096221b9b7c4
Thiti Dev <thiti.mwk.main@gmail.com>
2020-06-18 18:02:09 +0700

    :gem: [entry]: Call LogSetting after Initialized, Created action reader :hourglass:

--- src/app/entry.go
+++ src/app/entry.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
+	"time"
 
 	"./helpers/statrack"
 )
@@ -13,4 +16,15 @@ func main() {
 		fmt.Println("[USAGE]: => go run main.go --site=http://google.com,http://twitter.com --interval=5000")
 	}
 	statrack.InitializeConfigFromString(os.Args[1:])
+	statrack.LogSettings()
+	fmt.Print("Proceed tracking? [TYPE y OR n] : ")
+	reader := bufio.NewReader(os.Stdin) //create new reader
+	actionString, _ := reader.ReadString('\n')
+	if strings.Contains(actionString, "y") {
+		// Proceed the tracker
+	} else {
+		fmt.Println("Exiting .... ")
+		time.Sleep(1 * time.Second)
+		os.Exit(0)
+	}
 }
