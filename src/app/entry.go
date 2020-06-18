65af916df29dacbfc8996a6412041617393092b6
Thiti Dev <thiti.mwk.main@gmail.com>
2020-06-18 17:45:49 +0700

    :zap: [entry]: Passing args to the statrack with the skipped first arg

--- src/app/entry.go
+++ src/app/entry.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"./helpers/statrack"
 )
 
 func main() {
@@ -10,4 +12,5 @@ func main() {
 	if numOfSites <= 0 {
 		fmt.Println("[USAGE]: => go run main.go --site=http://google.com,http://twitter.com --interval=5000")
 	}
+	statrack.InitializeConfigFromString(os.Args[1:])
 }
